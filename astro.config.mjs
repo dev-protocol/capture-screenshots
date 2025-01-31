09120047b55e27da83795862488d0add9360da89
Aggre <hiroyuki.aggre@gmail.com>
2025-01-31 18:28:37 +0900

    add awsAmplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config'
 
 import vercel from '@astrojs/vercel/serverless'
 import netlify from '@astrojs/netlify'
+import awsAmplify from 'astro-aws-amplify'
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: process.env.NETLIFY ? netlify() : vercel({ maxDuration: 300 }),
+	adapter: process.env.NETLIFY ? netlify() : awsAmplify(),
 })
