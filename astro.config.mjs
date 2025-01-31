35ce4a7a4e7333c97f350b519a0b404dea7efc27
Aggre <hiroyuki.aggre@gmail.com>
2025-01-31 18:42:57 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config'
 
 import vercel from '@astrojs/vercel/serverless'
-import netlify from '@astrojs/netlify'
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: process.env.NETLIFY ? netlify() : vercel({ maxDuration: 300 }),
+	adapter: process.env.NETLIFY
+		? netlify({ edgeMiddleware: false })
+		: vercel({ maxDuration: 300 }),
 })
