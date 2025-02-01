372e061871022620e7e3afa0ff7194bc244f817b
Aggre <hiroyuki.aggre@gmail.com>
2025-02-01 23:24:16 +0900

    update

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
-import packageJson from './package.json' with { type: 'json' }
+import packageJson from './package.json'
 
 export default {
 	external: [
