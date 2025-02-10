0955b06e6985e5af35242f239a6cbcc6c1533637
aggre <hiroyuki.aggre@gmail.com>
2025-02-10 23:05:53 +0900

    using puppeteer-extra-plugin-stealth

--- src/libs/chromium.ts
+++ src/libs/chromium.ts
@@ -1,5 +1,8 @@
 import type { Browser, PuppeteerLaunchOptions } from 'puppeteer-core'
-import puppeteer from 'puppeteer-core'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+
+puppeteer.use(StealthPlugin())
 
 export const Chromium = (() => {
 	const instances: WeakMap<PuppeteerLaunchOptions, Browser> = new WeakMap()
